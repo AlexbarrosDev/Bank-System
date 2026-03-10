695162b1876ce3c5d149f868db74dbedda0ad6cf
Alex Barros <alexbarros.dev@gmail.com>
2026-03-10 07:42:57 -0300

    chore: updating spring boot version

--- src/main/resources/BankSystemApplication.java
+++ src/main/resources/BankSystemApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BankSystemApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(BankSystemApplication.class, args);
-    }
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankSystemApplication.class, args);
+	}
+
 }
